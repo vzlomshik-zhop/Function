e4897034922ad9dd824ea50304a9ea6ef2ef79ed
vzlomshik-zhop <55409370+vzlomshik-zhop@users.noreply.github.com>
2019-12-03 20:30:11 +0200

    Update 4-methods.js

--- Exercises/4-methods.js
+++ Exercises/4-methods.js
@@ -1,21 +1,29 @@
 'use strict';
 
 const methods = iface => {
-  // Introspect all properties of iface object and
-  // extract function names and number of arguments
-  // For example: {
-  //   m1: x => [x],
-  //   m2: function (x, y) {
-  //     return [x, y];
-  //   },
-  //   m3(x, y, z) {
-  //     return [x, y, z];
-  //   }
-  // will return: [
-  //   ['m1', 1],
-  //   ['m2', 2],
-  //   ['m3', 3]
-  // ]
+  let arr = [];
+  const n = iface.length;
+  let i = 0;
+  for (let [key, value] of Object.entries(iface)) {
+    arr.push();
+    arr[i++] = [key, value.length];
+  }
+  const m = arr.length;
+  for (let i = 0; i < m; i++)
+    console.log(arr[i]);
 };
+
+const iface = {
+  m1: x => [x],
+  m2: function (x, y) {
+    return [x, y];
+  },
+  m3(x, y, z) {
+    return [x, y, z];
+  }
+}
+
+
+methods(iface);
 
 module.exports = { methods };
